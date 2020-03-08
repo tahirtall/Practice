eb19710ef9006538efb8d32fcbe9e9bad5b470a0
Tahir Uzun <tahirtall46@gmail.com>
2020-03-07 22:55:46 -0600

    Initiated the backtracking structure for Knight's Tour game

---
+++ Backtracking-Java/backtracking.java
@@ -0,0 +1,29 @@
+class KnightTour {
+
+    static int N = 8;
+    
+    // A utility function to check if i, j are valid
+    // index for N*N chessboard.
+    static boolean isSafe(int x, int y, int[] board) {
+        // TODO
+        return true;
+    }
+    
+    // A utility function to print the solution matrix board[N][N]
+    static void printSolution(int[] board) {
+        // TODO 
+    }
+
+    // Function that runs the utility function
+    static boolean SolveKT() {
+        // TODO
+        return true;
+    }
+
+    // The Utility function itself.
+    static boolean solveKTUtil(int x, int y, int movei, int sol[][], int xMove[], int yMove[]) {
+        //TODO
+        return true;
+    }
+    
+}
