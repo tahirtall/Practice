fcae789ac7c176649bfc64c4194629f74046d766
Tahir Uzun <tahirtall46@gmail.com>
2020-03-07 23:08:15 -0600

    finished wiriting the isSafe function

--- Backtracking-Java/backtracking.java
+++ Backtracking-Java/backtracking.java
@@ -1,12 +1,22 @@
 class KnightTour {
-
+    
+    /* 
+     * Knight's Tour is a game where you mimic the move the knight makes in Chess, but this time the player is given
+     * N * N grid, and the objective is to visit every square in the grid only once to traverse through the whole grid.
+     *
+     * I will be using backtracking technique to accomplish this.
+     * Wish me good luck :)
+     */
+    
     static int N = 8;
     
     // A utility function to check if i, j are valid
     // index for N*N chessboard.
     static boolean isSafe(int x, int y, int[] board) {
-        // TODO
-        return true;
+        if(x >= -1 && y >= -1 && board.length == N) {
+            return true;
+        }
+        return false;
     }
     
     // A utility function to print the solution matrix board[N][N]
