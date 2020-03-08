13eaf85004e31f7a379e134c6e46a1e7a681429b
Tahir Uzun <tahirtall46@gmail.com>
2020-03-08 00:49:44 -0600

    The function runs, but won't print anything. It keeps running

--- Backtracking-Java/backtracking.java
+++ Backtracking-Java/backtracking.java
@@ -12,15 +12,15 @@ class KnightTour {
     
     // A utility function to check if i, j are valid
     // index for N*N chessboard.
-    static boolean isSafe(int x, int y, int[] board) {
-        if(x >= -1 && y >= -1 && board.length == N) {
+    static boolean isSafe(int x, int y, int[][] board) {
+        if(x >= 0 && x < N && y >= 0 && y < N && board[x][y] == -1) {
             return true;
         }
         return false;
     }
     
     // A utility function to print the solution matrix board[N][N]
-    static void printSolution(int[][] board) {
+    static void printSolution(int board[][]) {
         for (int i=0; i<N; i++) {
             for(int j=0; j<N; j++) {
                 System.out.print(board[i][j] + " ");
@@ -31,33 +31,51 @@ class KnightTour {
 
     // Function that runs the utility function
     static boolean SolveKT() {
-        int[][] board = new int[N][N];
+        int[][] board = new int[8][8];
         for (int i=0; i<N; i++) {
             for (int j=0; j<N; j++) {
                 board[i][j] = -1;
             }
         }
         
-        int[] xMove = {-1, 1, 2, -2, -1, -2, 2, 1};
-        int[] yMove = {2, 2, 1, 1, -2, -1, -1, -2};
+        int xMove[] = {-1, 1, 2, -2, -1, -2, 2, 1};
+        int yMove[] = {2, 2, 1, 1, -2, -1, -1, -2};
 
-        board[N][N] = 0;
+        board[0][0] = 0;
 
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<N; j++) {
-                if(!solveKTUtil(x, y, movei, sol, xMove, yMove)) {
-                    System.out.print("Can't do that");
-                }
-                return true;
-            }
+        if(!solveKTUtil(0,0,1, board, xMove, yMove)) {
+            System.out.print("Solution is invalid");
+            return false;
         }
-        return false;
+        else {
+            printSolution(board);
+        }
+        return true;
     }
 
     // The Utility function itself.
-    static boolean solveKTUtil(int x, int y, int movei, int sol[][], int xMove[], int yMove[]) {
-        //TODO
-        return true;
+    static boolean solveKTUtil(int x, int y, int movei, int board[][], int xMove[], int yMove[]) {
+        int next_x, next_y;
+        if (movei == N*N) {
+            return true;
+        }
+        for (int i=0; i<N; i++) {
+            next_x = x + xMove[i];
+            next_y = y + yMove[i];
+            if(isSafe(next_x, next_y, board)) {
+                board[next_x][next_y] = movei;
+                if(solveKTUtil(next_x, next_y, movei+1, board, xMove, yMove)) {
+                    return true;
+                }
+                else {
+                    board[next_x][next_y] = -1;
+                }
+            }
     }
-    
+    return false;
+    }
+
+    public static void main(String[] args) {
+        SolveKT();
+    }
 }
